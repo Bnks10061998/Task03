09d9ba9ea9238ad81d8ba05939f82c66c1609e91
Bnks10061998 <banakasu4scot@gmail.com>
2024-04-19 12:45:38 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Day 3 Task
+
+1. Comparing the two JSON have same Properties without order.
+    1. Program in the file[Order.js](./Order.js)
+2. Display Flags name for all the countries data.
+    1. created the html file and link the script.js
+    2. In script.js get to add the code for[restcountries API](https:// restcountries.com/v3.1/all) and excute all countries flags
+    3. The code location is [Flags](./Flags/) folder.
+3. Display Name,region, Subregion,population
+    1. created the html file and link the script.js
+    2. In script.js get to add the code for[restcountries API](https:// restcountries.com/v3.1/all) and excute all countries name,region,subregion,population
+    3. The code location is [countries details](./countries%20details/) folder.
---
+++ countries details/script2.js
@@ -0,0 +1,31 @@
+
+const API_URL = "https://restcountries.com/v3.1/all";
+console.log(API_URL);
+
+function reqListener()
+{
+    const countriesData = JSON.parse(this.responseText);
+    console.log(countriesData);
+    for(let ind=0;ind<countriesData.length;ind++)
+    {
+        
+        console.log(countriesData[ind].name.common, countriesData[ind].region, countriesData[ind].subregion,countriesData[ind].population);
+    }
+
+   // console.log(typeof this.responseText)
+}
+const req = new XMLHttpRequest();
+req.addEventListener("load", reqListener);
+req.open("GET", API_URL);
+req.send();
+
+/*function reqListener()
+{
+    const countriesData = JSON.parse(this.responseText);
+    console.log(countriesData);
+    for(let ind=0;ind<countriesData.length;ind++)
+    {
+        console.log(countriesData[ind].flag.png)
+    }
+}
+*/
